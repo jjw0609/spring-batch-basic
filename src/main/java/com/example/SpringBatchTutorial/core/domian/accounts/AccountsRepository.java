acda56aa4e31db9b517de9feb7405a438b65b718
wjdwodnd93 <jjw_0609@naver.com>
2024-08-07 21:08:31 +0900

    DB 데이터 이관하기

---
+++ src/main/java/com/example/SpringBatchTutorial/core/domian/accounts/AccountsRepository.java
@@ -0,0 +1,8 @@
+package com.example.SpringBatchTutorial.core.domian.accounts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountsRepository extends JpaRepository<Accounts, Integer> {
+
+
+}
---
+++ src/main/java/com/example/SpringBatchTutorial/core/domian/orders/OrdersRepository.java
@@ -0,0 +1,8 @@
+package com.example.SpringBatchTutorial.core.domian.orders;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdersRepository extends JpaRepository<Orders, Integer> {
+
+
+}
